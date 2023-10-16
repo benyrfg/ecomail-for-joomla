04aac85492d1d1c46cf1def437fe8642e1aad49e
Petr <beny.rfg@gmail.com>
2023-10-16 20:00:50 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ecomail for Joomla
+A plugin for the integration of Joomla registration form with Ecomail.
+Subscribe users during registration on the website.
+
+Features:
+- automatic od consent mode (user may be subscribed automatically or by clicking on checkbox)
+- name and surname collection
+
---
+++ ecomail.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * @version    1.0.0
+ * @package    Ecomail (plugin)
+ * @author     Petr Benes - https://petben.cz
+ * @copyright  Copyright (c) 2023 Petr Benes. All rights reserved.
+ * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ */
+
+
+// no direct access
+defined( '_JEXEC' ) or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\User\User;
+use Joomla\CMS\Factory;
+
+
+class PlgUserEcomail extends CMSPlugin  
+{
+
+	 public function onUserAfterSave($user, $isnew, $success, $msg)
+	 {
+		if (!$success)
+		{
+			return;
+		}
+
+
+        $key =              $this->params->get('ecomailapikey', 0);
+        $listID =           $this->params->get('listID', 0);
+        $source =           $this->params->get('source', 'API');
+        $autoSubscribe =    filter_var($this->params->get('autoSubscribe', 'true'), FILTER_VALIDATE_BOOLEAN);
+        $saveSplitName =    filter_var($this->params->get('saveSplitName', 'false'), FILTER_VALIDATE_BOOLEAN);
+        $updateExisting =   filter_var($this->params->get('updateExisting', 'false'), FILTER_VALIDATE_BOOLEAN);
+        $resubscribe =      filter_var($this->params->get('resubscribe', 'false'), FILTER_VALIDATE_BOOLEAN);
+        $skipConfirmation = filter_var($this->params->get('skipConfirmation', 'false'), FILTER_VALIDATE_BOOLEAN);
+        $autoresponders =   filter_var($this->params->get('autoresponders', 'false'), FILTER_VALIDATE_BOOLEAN);
+
+        function splitName($fullName) {
+            $words = explode(' ', $fullName);
+            $firstName = $words[0];
+            $lastName = implode(' ', array_slice($words, 1));
+            return array('firstName' => $firstName, 'lastName' => $lastName);
+        }
+
+
+        if($isnew === true AND $success === true){
+
+            if($autoSubscribe === false){
+                $db = Factory::getDbo();
+                $query = $db
+                    ->getQuery(true)
+                    ->select('value')
+                    ->from($db->quoteName('#__fields_values'))
+                    ->where($db->quoteName('item_id') . " = " . $db->quote($user['id']))
+                    ->where($db->quoteName('value') . " = " . $db->quote('subscribe'));
+    
+                $db->setQuery($query);
+                $result = $db->loadResult();
+            } elseif($autoSubscribe === true){
+                $result = "subscribe";
+            }            
+
+            JLog::add($result, JLog::ERROR, 'jerror');
+
+            if($result == "subscribe"){
+
+                $params = array(
+                    "subscriber_data" => array (               
+                        "email" => $user['email'],
+                    ),                      
+                    "trigger_autoresponders" => false,
+                    "update_existing" => true,
+                    "resubscribe" => true
+                      
+                );
+
+                // save split name
+                if($saveSplitName === true ){
+                    
+                    $fullName = splitName($user['name']);
+
+                    if($fullName['firstName'] != ""){
+                        $params["subscriber_data"]["name"] = $fullName['firstName'];
+                    }
+                    if($fullName['lastName'] != ""){
+                        $params["subscriber_data"]["surname"] = $fullName['lastName'];
+                    }
+                }              
+                
+                // send to Ecomail
+                require_once __DIR__ . '/includes/Ecomail.php';
+                $ecomail = new Ecomail($key);        
+                $ecomail->addSubscriber($listID, $params);              
+               
+            }
+            
+        }
+        
+        //JLog::add(json_encode($ecomail), JLog::ERROR, 'jerror');
+        //JFactory::getApplication()->enqueueMessage(json_encode($ecomail));
+        
+
+		return;
+	}
+
+
+}
+?>
+
